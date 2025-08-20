bb982fdf934dcc59483ade4bb0ac31fbbed2ca70
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2025-08-20 23:02:21 +0200

    chore: 🚧 Add pointer

--- inquirer_textual/select.py
+++ inquirer_textual/select.py
@@ -1,6 +1,16 @@
 from textual.app import App, ComposeResult
 from textual.widgets import ListView, Label, ListItem
 
+class ChoiceLabel(Label):
+    def __init__(self, text: str):
+        super().__init__('  ' + text)
+        self.text = text
+
+    def add_pointer(self):
+        self.update(f'\u276f {self.text}')
+
+    def remove_pointer(self):
+        self.update(f'  {self.text}')
 
 class SelectApp(App[str]):
     INLINE_PADDING = 0
@@ -16,7 +26,7 @@ class SelectApp(App[str]):
         self.message = message
         self.choices = choices
         self.list_view: ListView | None = None
-        self.selected: str | None = None
+        self.selected: ChoiceLabel | None = None
 
     def on_mount(self) -> None:
         self.app.styles.background = 'black 0%'
@@ -26,13 +36,20 @@ class SelectApp(App[str]):
         self.list_view.styles.background = 'black 0%'
         self.list_view.styles.height = len(self.choices)
 
+    def on_list_view_highlighted(self, event: ListView.Highlighted) -> None:
+        if self.selected:
+            self.selected.remove_pointer()
+        label = event.item.query_one(ChoiceLabel)
+        label.add_pointer()
+        self.selected = label
+
     def on_list_view_selected(self, event: ListView.Selected) -> None:
         self.exit(self.choices[event.index])
 
     def compose(self) -> ComposeResult:
         items: list[ListItem] = []
         for choice in self.choices:
-            list_item = ListItem(Label(choice))
+            list_item = ListItem(ChoiceLabel(choice))
             items.append(list_item)
         self.list_view = ListView(*items)
         yield self.list_view
